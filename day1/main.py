85239eef6ec7cdb3b00ab9f950487a10f08c5322
GrimalDev <gb.titou2b@gmail.com>
2025-12-02 10:48:02 +0100

    day1 part 2

--- day1/main.py
+++ day1/main.py
@@ -1,19 +1,24 @@
 def main():
-
     result1 = 0
+    result2 = 0
     dial = 50
     with open("day1/input.txt", "r") as f:
         lines = f.read().strip().split("\n")
         for line in lines:
             sign = 2 * int(line[0] == "R") - 1
             rotations = int(line[1:])
-            dial = (dial + sign * rotations) % 100
-            print(dial)
+
+            newDial = dial + sign * rotations
+
+            if sign == 1:
+                result2 += newDial // 100
+            else:
+                result2 += -((newDial - 1) // 100) - (1 if dial == 0 else 0)
+
+            dial = newDial % 100
+
             if dial == 0:
                 result1 += 1
-
-    # Part 2
-    result2 = 0
 
     print(f"Part 1: {result1}")
     print(f"Part 2: {result2}")
