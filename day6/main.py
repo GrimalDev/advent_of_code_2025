dfefd307791c539b827eb60a63cf089d893435c6
GrimalDev <gb.titou2b@gmail.com>
2025-12-04 13:49:12 +0100

    template changes

--- day6/main.py
+++ day6/main.py
@@ -1,13 +1,18 @@
-def part1():
+def getData():
     with open("day6/input.txt", "r") as f:
-        lines = f.read().strip().split("\n")
+        return f.read().strip().split("\n")
 
 
-def part2():
-    with open("day6/input.txt", "r") as f:
-        lines = f.read().strip().split("\n")
+def part1(data):
+    pass
+
+
+def part2(data):
+    pass
 
 
 def main():
-    print(f"Part 1: {part1()}")
-    print(f"Part 2: {part2()}")
+    data = getData()
+
+    print(f"Part 1: {part1(data)}")
+    print(f"Part 2: {part2(data)}")
